963d3993b605a325549c926a4e43976cf7c6bc1b
Giddy-K <gideonkipamet@gmail.com>
2023-11-02 23:59:58 +0300

    added bool and number file

---
+++ 001Intro/number.js
@@ -0,0 +1,7 @@
+var userId = 34353737;
+console.log(userId.toExponential());
+console.log(userId.toFixed());
+console.log(userId.toLocaleString());
+console.log(userId.toString());
+console.log(userId.toPrecision());
+console.log(userId.valueOf());
---
+++ 001Intro/number.ts
@@ -0,0 +1,8 @@
+let userId = 34353737;
+
+console.log(userId.toExponential());
+console.log(userId.toFixed());
+console.log(userId.toLocaleString());
+console.log(userId.toString());
+console.log(userId.toPrecision());
+console.log(userId.valueOf());
