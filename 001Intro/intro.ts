011dc9b7b421084b819d6ef66513d38747290b28
Giddy-K <gideonkipamet@gmail.com>
2023-11-02 23:42:52 +0300

    added varibles

--- 001Intro/intro.ts
+++ 001Intro/intro.ts
@@ -1,7 +1,10 @@
-let user = {name: "Gideon", age: 24}//ignore the error which appered after generating the .js file.
+//ignore the error which appered after generating the .js file.
+let user = {name: "Gideon", age: 24}
+
 
 console.log("Gideon");
 console.log(user.name);
 
 //commands in terminal
 // 1. tsc intro.ts to generate the .js file
+
