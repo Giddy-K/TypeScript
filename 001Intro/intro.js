eb68230066bfc25bcb8d2b59e2b3f0a5f2f7e23f
Giddy-K <gideonkipamet@gmail.com>
2023-11-02 23:22:35 +0300

    Getting started with TypeScript

---
+++ 001Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Gideon", age: 24 };
+console.log("Gideon");
+console.log(user.name);
---
+++ 001Intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {name: "Gideon", age: 24}//ignore the error which appered after generating the .js file.
+
+console.log("Gideon");
+console.log(user.name);
+
+//commands in terminal
+// 1. tsc intro.ts to generate the .js file
