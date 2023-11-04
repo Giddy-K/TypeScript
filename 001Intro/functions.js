6c4b4a8002e0ed9b30e50ecf552f52abdf5db5e6
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 12:29:43 +0300

    More functions

--- 001Intro/functions.js
+++ 001Intro/functions.js
@@ -1,5 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
+    //return "Hello"
 }
 function getUpper(value) {
     return value.toUpperCase();
@@ -8,7 +11,28 @@ function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
-addTwo(5);
+var myValue = addTwo(5);
 getUpper("SuperUser");
 signUpUser("SuperUser", "u.anonymous@gmail.com", true);
 loginUser("SuperUser", "u.anonymous@gmail.com", true);
+// function getValue(nyVal: number) {
+//     if (myVal > 5){
+//         return true;
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "Siderman", "IronMan"];
+//const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return ("Hey ".concat(hero));
+    //return 2
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- 001Intro/functions.ts
+++ 001Intro/functions.ts
@@ -1,6 +1,6 @@
-function addTwo(num: number) {
-    //return num + 2;
-    return "Hello"
+function addTwo(num: number): number {
+    return num + 2;
+    //return "Hello"
 }
 
 function getUpper(value: string) {
@@ -17,3 +17,33 @@ getUpper("SuperUser");
 
 signUpUser("SuperUser", "u.anonymous@gmail.com", true);
 loginUser("SuperUser", "u.anonymous@gmail.com", true);
+
+// function getValue(nyVal: number) {
+
+//     if (myVal > 5){
+//         return true;
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heros = ["thor", "Siderman", "IronMan"]
+//const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return (`Hey ${hero}`);
+    //return 2
+})
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
+
+export { }
