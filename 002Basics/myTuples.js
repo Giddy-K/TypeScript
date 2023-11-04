a58eb2a199da41942bd1caf439f4b3995ed33e34
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 14:51:16 +0300

    Added tuples

---
+++ 002Basics/myTuples.js
@@ -0,0 +1,7 @@
+// const user: (string | number)[] = [1, "hc"]
+//Order is important!!
+var user;
+user = ["hc", 131, true];
+var rgb = [255, 123, 222];
+var newUser = [1222, "example@gmail.com"];
+newUser[1] = "hc.com";
---
+++ 002Basics/myTuples.ts
@@ -0,0 +1,14 @@
+// const user: (string | number)[] = [1, "hc"]
+//Order is important!!
+let user: [string, number, boolean]
+
+user = ["hc", 131, true]
+
+let rgb: [number, number, number] = [255, 123, 222]
+
+type User = [number, string]
+
+const newUser: User = [1222, "example@gmail.com"]
+
+newUser[1] = "hc.com"
+
