60ee67bdc872ff6fb7bb434d9bdc458c7ee814be
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 14:15:39 +0300

    Arrarys

---
+++ 002Basics/myArray.js
@@ -0,0 +1,10 @@
+var superHeros = [];
+//const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+];
+superHeros.push("spiderman");
+heroPower.push(10);
+allUsers.push({ name: "", isActive: true });
---
+++ 002Basics/myArray.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+//const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string,
+    isActive: boolean,
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+]
+
+superHeros.push("spiderman");
+heroPower.push(10);
+
+allUsers.push({ name: "", isActive: true });
+
+export{}
