dc5e045876df455b9d98f7348c753a28b6b0e284
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 16:19:49 +0300

    minor changes

--- 002Basics/myInterfaces.ts
+++ 002Basics/myInterfaces.ts
@@ -8,16 +8,28 @@ interface User {
     getCoupon(couponName: string, value: number): number,
 }
 
-const SuperUser: User = {
+//Reopening of the interface
+interface User {
+    githubToken: string
+}
+
+//Use extends or inheritance
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+const SuperUser: Admin = {
     _id: 1323,
     email: "h@h.com",
     userId: 12313,
+    role: 'admin',
     startTrial: () => {
         return 'started';
     },
     getCoupon: (couponName: "hitesh10", value: 10) => {
         return 10;
     },
+    githubToken: "",
 };
 
 SuperUser.email = "superuser@gmail.com";
