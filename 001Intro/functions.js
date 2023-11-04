2ff8fe93636e6c7e038f9becd8fd624cab5fdc72
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 12:09:54 +0300

    Additions to functions in function.ts file

--- 001Intro/functions.js
+++ 001Intro/functions.js
@@ -1,6 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
 }
+function getUpper(value) {
+    return value.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
 addTwo(5);
+getUpper("SuperUser");
+signUpUser("SuperUser", "u.anonymous@gmail.com", true);
+loginUser("SuperUser", "u.anonymous@gmail.com", true);
--- 001Intro/functions.ts
+++ 001Intro/functions.ts
@@ -1,7 +1,19 @@
-function addTwo(num: number){
-    return num + 2
+function addTwo(num: number) {
+    //return num + 2;
+    return "Hello"
 }
 
-addTwo(5)
+function getUpper(value: string) {
+    return value.toUpperCase();
+}
 
-export{}
+function signUpUser(name: string, email: string, isPaid: boolean) { }
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => { }
+
+
+let myValue = addTwo(5);
+getUpper("SuperUser");
+
+signUpUser("SuperUser", "u.anonymous@gmail.com", true);
+loginUser("SuperUser", "u.anonymous@gmail.com", true);
