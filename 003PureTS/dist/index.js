0ba4eb9b00c61a509e7d0bd28c656ed466e9b146
Giddy-K <gideonkipamet@gmail.com>
2023-11-04 17:54:53 +0300

    Added access modifiers

--- 003PureTS/dist/index.js
+++ 003PureTS/dist/index.js
@@ -14,9 +14,29 @@ class User {
         this.email = email;
         this.name = name;
         this.userId = userId;
+        this._courseCount = 1;
         this.city = "Nairobi";
+    }
+    //private in methods
+    deleteToken() {
+        console.log("Deleting token");
+    }
+    //getter
+    get getAppleEmail() {
+        return `${this.name} has an apple email of ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    //You cannot set a variable or void in a setter //!set courseCount(courseNum): void{} 
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const SuperUser = new User("superuser@example.com", "SuperUser", "12345");
 // SuperUser.city = "Nairobi" //!!READ-ONLY!!
 //SuperUser.name //!private
+// SuperUser.deleteToken()//!private
--- 003PureTS/scr/index.ts
+++ 003PureTS/scr/index.ts
@@ -9,16 +9,40 @@
 //     }
 // }
 class User {
+
+    private _courseCount = 1
+
     readonly city: string = "Nairobi"
     constructor(
         public email: string,
         public name: string,
-        private userId : string,
+        private userId: string,
     ) {
 
+    }
+    //private in methods
+    private deleteToken() {
+        console.log("Deleting token")
+    }
+
+    //getter
+    get getAppleEmail(): string {
+        return `${this.name} has an apple email of ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+    //You cannot set a variable or void in a setter //!set courseCount(courseNum): void{} 
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 
 const SuperUser = new User("superuser@example.com", "SuperUser", "12345")
 // SuperUser.city = "Nairobi" //!!READ-ONLY!!
 //SuperUser.name //!private
+// SuperUser.deleteToken()//!private
