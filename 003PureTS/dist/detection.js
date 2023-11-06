01b9245ee4eb74686af188bf1aac66bd95f6690a
Giddy-K <gideonkipamet@gmail.com>
2023-11-06 21:25:34 +0300

    Finished Narrowing

--- 003PureTS/dist/detection.js
+++ 003PureTS/dist/detection.js
@@ -25,3 +25,49 @@ function printAll(strs) {
         }
     }
 }
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+    return false;
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "Fish food";
+    }
+    else {
+        pet;
+        return "Bird food";
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.width * shape.lenght;
+        default:
+            const _defaultforshpe = shape;
+            return _defaultforshpe;
+    }
+}
--- 003PureTS/scr/detection.ts
+++ 003PureTS/scr/detection.ts
@@ -69,3 +69,45 @@ function getFood(pet: Fish | Bird) {
         return "Bird food";
     }
 }
+
+interface Circle{
+    kind: "circle",
+    radius: number
+}
+
+interface Square{
+    kind: "square",
+    side: number
+}
+
+interface Rectangle{
+    kind: "rectangle",
+    width: number,
+    lenght: number
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape){
+    if(shape.kind === "circle"){
+        return Math.PI * shape.radius ** 2
+    }
+    // return shape.side * shape.side
+}
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+
+        case "square":
+            return shape.side * shape.side;
+
+        case "rectangle":
+            return shape.width * shape.lenght;
+            
+        default:
+            const _defaultforshpe: never = shape
+            return _defaultforshpe
+    }
+}
