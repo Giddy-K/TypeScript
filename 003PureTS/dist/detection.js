0bf41b4a1074ef5f287b371dea7d90987c2636a3
Giddy-K <gideonkipamet@gmail.com>
2023-11-06 21:13:38 +0300

    Type narrowing

--- 003PureTS/dist/detection.js
+++ 003PureTS/dist/detection.js
@@ -25,3 +25,30 @@ function printAll(strs) {
         }
     }
 }
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+    return false;
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "Fish food";
+    }
+    else {
+        pet;
+        return "Bird food";
+    }
+}
--- 003PureTS/scr/detection.ts
+++ 003PureTS/scr/detection.ts
@@ -1,12 +1,12 @@
-function detectType(val: number | string){
+function detectType(val: number | string) {
     if (typeof val === 'number') {
-       return val.toFixed
+        return val.toFixed
     }
     return val.toLowerCase
 }
 
-function provideId(id : string | null){
-    if(!id){
+function provideId(id: string | null) {
+    if (!id) {
         console.log("Please provide an ID!");
         return
     }
@@ -15,13 +15,57 @@ function provideId(id : string | null){
 
 //!Don not do this!!
 function printAll(strs: string | string[] | null) {
-    if(strs){
-        if(typeof strs === "object"){
-            for (const s of strs){
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
                 console.log(s);
             }
         } else if (typeof strs === "string") {
             console.log(strs);
         }
+    }
+}
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+    return false;
+}
+
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return "Fish food";
+    } else {
+        pet
+        return "Bird food";
     }
 }
